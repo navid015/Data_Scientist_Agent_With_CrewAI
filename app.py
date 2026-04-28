a0dd08ebff90f04e5ce0f2fd90f1af8e979b4d54
navid015 <navid_pme@hotmail.com>
2026-04-27 19:02:53 -0500

    Fix launch config and pin gradio versions

--- app.py
+++ app.py
@@ -424,4 +424,4 @@ with gr.Blocks(css=CSS, title="CrewAI AutoML", theme=gr.themes.Base()) as demo:
 
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=7860)
